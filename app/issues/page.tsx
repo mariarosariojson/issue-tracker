606c784a2e127289f1af6250ffd7bb62e15c3057
Maria Rosario <mariarosariojohansson@gmail.com>
2023-12-14 16:10:54 +0100

    Radix UI is set up

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,13 @@
+"use client";
 import React from "react";
+import { Button } from "@radix-ui/themes";
 
 const IssuesPage = () => {
-  return <div>IssuesPage</div>;
+  return (
+    <div>
+      <Button>Add new issue</Button>
+    </div>
+  );
 };
 
 export default IssuesPage;
